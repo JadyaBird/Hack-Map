6e456e46a7376a8c22c853af89f2759b9b1a4496
mecodegood <mecodegood@gmail.com>
2014-09-20 02:25:10 -0400

    base mac application

---
+++ HackMap/HackMap/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HackMap
+//
+//  Created by Jenna Godwin on 2014-09-20.
+//  Copyright (c) 2014 Jenna Godwin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
